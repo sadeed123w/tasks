1c4a409fc50f23056f77529277ad443a489eb43f
sadeed123w <147578821+sadeed123w@users.noreply.github.com>
2023-10-11 09:54:59 +0500

    Add files via upload

---
+++ #sadeed.cpp
@@ -0,0 +1,81 @@
+NAME:SADEED ARIF
+ROLL:455385
+
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+//{
+// cout<<"TASK 1\n";
+// float age;
+// cout<<"enter the age of person\n";
+// cin>>age;
+// if (age>18) {
+// cout<<"The person is eligible to vote";
+//}
+// else{
+// cout<<"the person is not eligible to vote";
+//}
+// return 0;
+//}
+
+
+//{
+//  cout<<"TASK 2\n";
+//	float a;
+//	cout<<"enter value of a\n";
+//	cin>>a;
+//	if (10<=a<=50) {
+//		cout<<"10<=x<=50";
+//	}
+//	else{
+//	cout<<"10>x>50";
+//	}
+//  return 0;
+//}
+
+
+//{
+//	cout<<"TASK 3\n";
+//float num1,num2;
+//cout<<"enter 1st number"<<endl;
+//cin>>num1;
+//cout<<"enter 2nd number"<<endl;
+//cin>>num2;
+//if (num1>num2) { 
+//cout<<num1;
+//}
+//else {
+//if (num1<num2) {cout<<num2;
+//}
+//else{cout<<"num1=num2";
+//} 
+//}
+//    return 0;
+//}
+
+
+{
+	cout<<"TASK 4"<<endl;
+	float chem,phy,math;
+	cout<<"enter score obtained in maths out of 100"<<endl;
+	cin>>math;
+	cout<<"enter score obtained in phy out of 100"<<endl;
+	cin>>phy;
+	cout<<"enter score obtained in chem out of 100"<<endl;
+	cin>>chem;
+	float sum=math+phy+chem;
+	float ave=sum/3;
+	if (ave>60) {cout<<"score is above passing grades"<<endl;
+	}
+	else{if (ave<60) {cout<<"score is below pasing grades"<<endl;
+	}
+	else{cout<<"score is equal to passing grades"<<endl;
+	} 
+	return 0;
+	}
+	
+	return 0;
+	}	
